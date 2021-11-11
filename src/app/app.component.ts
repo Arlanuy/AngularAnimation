c05d1729e0b8f49fea953f6787564dd7e51847f6
Arlan Uy <gnhs.com@gmail.com>
2021-11-11 12:46:17 -0600

    first commit revised app component to work with a template

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
+
 import { Component } from '@angular/core';
 
+
+
 @Component({
+
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+  template: `
+
+        <p>Angular Animations</p>
+
+    `,
+
+    styles: [``]
+
 })
-export class AppComponent {
-  title = 'angulardesign';
-}
+
+export class AppComponent {}
